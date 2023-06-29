76c3a0fc30a4180d7a051332c9aeddd8610c52db
arthur chiquet <arthur.chiquet@gmail.com>
2023-06-29 13:40:55 +0200

    map

--- app/javascript/controllers/toggle_map_controller.js
+++ app/javascript/controllers/toggle_map_controller.js
@@ -4,12 +4,10 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["map", "list", "button"]
 
-  connect() {
-  }
-
   displayMap() {
     this.listTarget.classList.toggle("d-none")
     this.buttonTarget.classList.toggle("border-bottom")
     this.mapTarget.classList.toggle("d-none")
   }
+
 }
