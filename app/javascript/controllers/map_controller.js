a4018aa8b3a3a90d455abe49b3b29f1a9e68c93e
arthur chiquet <arthur.chiquet@gmail.com>
2023-06-30 12:13:03 +0200

    chat map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -35,6 +35,5 @@ export default class extends Controller {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-    this.map.resize()
   }
 }
